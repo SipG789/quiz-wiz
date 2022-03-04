8d1f79ac7c296ec04b933028368e3ca7f891a325
Sip <sipg789@gmail.com>
2022-03-03 20:20:30 -0600

    added basic html

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// TO DO: Show HTML title 
-// TO DO: Start button that I can click to start the quiz 
+// TODO: Show HTML 
+// TODO: Start button that I can click to start the quiz 
     // listening events for starting quiz (on click)
-// TO DO: timer starts when I see first question 
+// TODO: timer starts when I see first question 
     // setInterval to do countdown (global?)
-// TO DO: Display question and answers
-// TO DO: list of questions and answers (know which one is correct.... if else ??) 
+// TODO: Display question and answers
+// TODO: list of questions and answers (know which one is correct.... if else ??) 
     // listening event for user selecting answer from the choices 
-// TO DO: if I answer correct I am presented with another question
-    // TO DO: add score if they got it right (localstorage.set)
+// TODO: if I answer correct I am presented with another question
+    // TODO: add score if they got it right (localstorage.set)
         // do not deduct penalty time 
-    // TO DO: remove time if I get it wrong (10 sec)
+    // TODO: remove time if I get it wrong (10 sec)
         // alert! incorrect!
         //- move to next question but dont count as correct 
 // keep track of # correct 
-// TO DO: stop game when all are answered or timer reaches 0 
-// TO DO: save user score 
-    // TO DO: ask user for initials to save 
+// TODO: stop game when all are answered or timer reaches 0 
+// TODO: save user score 
+    // TODO: ask user for initials to save 
