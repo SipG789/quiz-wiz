153fa5c5d4277f89762c659e007a9f507fec0e74
Sip <sipg789@gmail.com>
2022-03-06 23:25:56 -0600

    stopping point and added ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # quiz-wiz
-Timed Quiz Application 
+## Timed Quiz Application 
+
+
+### In this application I attempted to create a Trivia Game in which the user is asked trivia questions about The Beatles. I was able to create a mock up website and I was able to have the application ask the user a couple of questions as well as create a timer and let users know the rules of the game. 
+
+### I used the following resources to help me build my code 
+```
+taskinator app 
+robot gladiators app 
+bootcamp lessons 
+Module 4
+```
+### I tried to stay away from any websites to try and create this with my own intellectual property and this was the result. It ain't much, but it's honest work. 
+
+The following image shows a screenshot of the application.
+![screenshot](./assets/trivia-quiz.png)
+
+## Accepted Criteria 
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+```
+
+The link to the website is here: 
+
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 // TO DO: Start button that I can click to start the quiz 
 var startQuiz = document.querySelector("#start-button");
+var nextQuestion = document.querySelector("#next-question");
+// var ansEl = document.querySelector("#page-form-answer");
 // setInterval to do countdown (global?)
 var timerEl = document.getElementById('countdown');
-
-countdown();    
-
+//var questionArrayElement = getElementById('page-form-question');
+// timer for game 
+countdown();
 //var formEl = document.querySelector("#questions-to-answer")
 setTimeout(function () {
     var initialPrompt = document.querySelector("#page-form-question");
@@ -14,22 +16,90 @@ setTimeout(function () {
     initialPrompt.append(promptCommand);
 }, 1000);
 
+
 // TODO: listening events for starting quiz (on click)
 var startTheQuiz = function (event) {
-
     // start the quiz 
     var questionEl = event.target;
     // when start button is clicked 
     if (questionEl.matches(".btn")) {
-        //   window.confirm("You are about to begin the quiz! Are you ready?");
         var firstEl = document.querySelector("#page-form-question");
         firstEl.textContent = "How many studio albums did The Beatles release?";
         firstEl.className = "page-form";
-        return questionEl;
-    };
+        var answerEl = document.querySelector("#answer-list-responses");
+        answerEl.textContent = "A. 21";
+        answerEl.className = "list-group";
+    }
 };
 
-// TODO: Display question and answers
+function totalQuestionList (event) {
+    var questionChoices = [questions];
+    for (var i = 0; i < questionChoices.length; i++) {
+        var secondEl = document.querySelector("#page-form-question");
+        secondEl.textContent = "How many members are in The Beatles?";
+        secondEl.className = "page-form";
+        var answerTwoEl = document.querySelector("#answer-list-responses");
+        var answerTwoEl = document.createElement("button");
+        answerTwoEl.textContent = "C. 4";
+        answerTwoEl.className = "list-group";
+    };
+    for (var i = 0; i < questionChoices.length; i++) {
+        var thirdEl = document.querySelector("#page-form-question");
+        thirdEl.textContent = "Which Beatles member plays the bass?";
+        thirdEl.className = "page-form";
+        var answerThreeEl = document.querySelector("#answer-list-responses");
+        var answerThreeEl = document.createElement('button');
+        answerThreeEl.textContent = "D. Paul";
+        answerThreeEl.className = "list-group";
+    };
+    endGame();
+    };
+
+
+var questions = [
+    {
+        question: "How many studio albums did The Beatles release?",
+        a: "21",
+        b: "19",
+        c: "12",
+        d: "32",
+        correct: "a",
+    },
+    {
+        question: "How many members are in The Beatles?",
+        a: "2",
+        b: "6",
+        c: "4",
+        d: "3",
+        correct: "c",
+    },
+    {
+        question: "Which Beatles member plays the bass?",
+        a: "John",
+        b: "George",
+        c: "Ringo",
+        d: "Paul",
+        correct: "d",
+    },
+    {
+        question: "Which Beatles song holds the record for the most versions covered by other artists?",
+        a: "Let It Be",
+        b: "Yesterday",
+        c: "Come Together",
+        d: "Eight Days A Week",
+        correct: "b",
+    },
+    {
+        question: "Which song contributed to the rumor that Paul had died?",
+        a: "Paul is Dead",
+        b: "Love Me Do",
+        c: "Strawberry Fields Forever",
+        d: "The Long & Winding Road",
+        correct: "c",
+    },
+];
+
+
 // TODO: timer starts when I see first question 
 function countdown() {
     var timeLeft = 90;
@@ -47,8 +117,8 @@ function countdown() {
             clearInterval(timeInterval);
         }
     }, 1000);
-}
-// TODO: list of questions and answers (know which one is correct.... if else ??) 
+};
+ 
 // listening event for user selecting answer from the choices 
 // TODO: if I answer correct I am presented with another question
 // TODO: add score if they got it right (localstorage.set)
@@ -58,12 +128,33 @@ function countdown() {
 //- move to next question but dont count as correct 
 // keep track of # correct 
 // TODO: stop game when all are answered or timer reaches 0 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your name?");
+    }
+};
+
+var playerInfo = {
+    name: getPlayerName(),
+}
 // TODO: save user score 
+var endGame = function () {
+var highScore = localStorage.getItem("highscore");
+if (highScore === null) {
+    highScore = 0;
+}
+if (countdown > highScore) {
+    localStorage.setItem("highscore", countdown);
+    localStorage.setItem("name", playerInfo.name);
+}
+};
 // TODO: ask user for initials to save 
 
 
 
 
 // TODO: add event listeners 
-//formEl.addEventListener("submit", nextQuestion);
 startQuiz.addEventListener("click", startTheQuiz);
+nextQuestion.addEventListener("click", totalQuestionList);
